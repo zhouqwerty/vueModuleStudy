28707a88c32ee0950f979ddefa009e8aa99b46c8
zhou <2891128968@qq.com>
2018-12-17 15:26:23 +0800

    2018.12.17 14:49 学习公司的新版vue组件方式

--- src/modules/header/header.vue
+++ src/modules/header/header.vue
@@ -1,7 +1,21 @@
 <template>
     <div>
         <h1>这是vue组件学习的界面</h1>
+        <Steps :current="2" size="small">
+            <Step title="已完成"></Step>
+            <Step title="进行中"></Step>
+            <Step title="待进行"></Step>
+            <Step title="待进行"></Step>
+        </Steps>
+        <Page :total="100" show-sizer></Page>
+        <!--<gaea-steps :current="2" size="small">-->
+            <!--<gaea-step title="已完成"></gaea-step>-->
+            <!--<gaea-step title="进行中"></gaea-step>-->
+            <!--<gaea-step title="待进行"></gaea-step>-->
+            <!--<gaea-step title="待进行"></gaea-step>-->
+        <!--</gaea-steps>-->
     </div>
+
 </template>
 <script>
     module.exports = {
